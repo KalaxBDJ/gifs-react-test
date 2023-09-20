2447450c183d3b774d0e3287b36377cdc0d2a19b
Santiago Salazar <kalaxdj@gmail.com>
2023-09-20 15:16:18 -0500

    preparing to deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base : 'C:/Users/kalax/OneDrive/Escritorio/gifs-app/dist/',
+  plugins: [react()]
 });
 
